7a30101b05546572f9536919229a116a3cce3710
magui <maguidifalco@gmail.com>
2018-10-16 14:52:33 -0300

    cambios

--- Clase 17 - 18/Alumno/Workshop final/src/js/controllers/peopleController.js
+++ Clase 17 - 18/Alumno/Workshop final/src/js/controllers/peopleController.js
@@ -1,49 +1,91 @@
-import { getData } from '../utils/ajax'
+import { getData } from '../utils/ajax';
 
+function peopleController() {
+	console.log('Se cargo la people ctrl 2');
 
-function peopleController () {
-  console.log('Se cargo la people ctrl 2')
-  
-  var tableBodyNode = $('#tableBody')
+	var tableBodyNode = $('#tableBody');
 
-  var seeMoreButton = $('#seeMore')
+	var seeMoreButton = $('#seeMore');
 
-  getData('https://swapi.co/api/people/', showPeople)
+	getData('https://swapi.co/api/people/', showPeople);
 
-  function showPeople (error, data) {
-    if (error) {
-      console.log('Falló algo ', error)
-    } else {
-      console.log('Ok los vehiculos son:', data)
-      var people = data.results
+	function showPeople(error, data) {
+		if (error) {
+			console.log('Falló algo ', error);
+		} else {
+			console.log('Ok los vehiculos son:', data);
+			var people = data.results;
 
-      var person
+			var person;
 
-      for (var i = 0; i < people.length; i++) {
-        person = people[i]
+			for (var i = 0; i < people.length; i++) {
+				person = people[i];
 
-        var url = person.url
+				var url = person.url;
 
-        url = url.replace('https://swapi.co/api/people/', '')
+				url = url.replace('https://swapi.co/api/people/', '');
 
-        var id = url.replace( '/', '')
+				var id = url.replace('/', '');
 
-        console.log(id)
+				console.log(person);
 
-        tableBodyNode.append('<tr><th scope="row">1</th><td>Mark</td><td>Otto</td><td>@mdo</td></tr>')
-      }
-      if (data.next) {
-        seeMoreButton.one('click', function () {
-          getData(data.next, showPeople)
-        })
-      } else {
-        seeMoreButton.remove()
-      }
-    }
-  }
+				tableBodyNode.append(
+					'<tr id="' + id +	'"><th scope="row">' +	id +	'</th><td>' +	person.name +	'</td><td>' +	genderTranslate +	person.gender +	'</td><td>' +	person.height +	' cm</td><td>' +	person.mass +'</td><td>' +	person.eye_color +	' <button type="button" class="btn btn-success"> Success </button> '
+				);
+			}
+			if (data.next) {
+				seeMoreButton.one('click', function() {
+					getData(data.next, showPeople);
+				});
+			} else {
+				seeMoreButton.remove();
+			}
+		}
+	}
 }
 
-export default peopleController
+function genderTranslate(gender) {
+	switch (gender) {
+		case 'male':
+			return 'masculino';
+			break;
 
+		case 'female':
+			return 'femenino';
+			break;
 
-  
+		case 'n/a':
+			return 'n/a';
+			break;
+
+		default:
+			return gender;
+			break;
+	}
+}
+
+var TRANSLATES = {
+	ES: {
+		blue: 'azules',
+		yellow: 'amarillos',
+		red: 'rojos'
+	},
+
+	EN: {
+		blue: 'blue',
+		yellow: 'yellow',
+		red: 'red'
+	},
+
+	PT: {
+		blue: 'azulsiño',
+		yellow: 'amarilliño',
+		red: 'rojiño'
+	}
+};
+
+function eyeTranslate(leng, eyeColor) {
+	return TRANSLATES[lang][eyeColor] || eyeColor;
+}
+
+export default peopleController;
